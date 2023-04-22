349349a64bd02ebf1581ffc935379f3d0d1587e8
ThirdPartyHugo <hpskate26@gmail.com>
2023-04-22 18:06:14 -0400

    h

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -3,40 +3,33 @@ import Header from "@/components/Header";
 import Head from "next/head";
 import { useEffect, useState } from "react";
 
-export default function Register(){
+export default function Register() {
 
-    useEffect(()=>
-    {
-        
+    useEffect(() => {
+
     }, [])
 
 
-    
 
-    return(
+
+    return (
         <>
-        <Header/>
-        <Head>
-        <script src="https://www.amilia.com/scripts/amilia-iframe.js" type="text/javascript"></script>
-      </Head>
-        
-        
-        <section className="banner section has-text-centered" style={{ height: "80vh" }}>
-                
-            <div id="amilia">
-            <iframe id="amilia-iframe" width="100%"
-           style={{width:'100%!important', 
-           border:'none!important', 
-       overflow:'hidden!important', 
-       visibility:'hidden'}}
-            scrolling="no" data-color-code="#2B91E1" src="https://www.amilia.com/store/en/urbanelementzone/shop/programs">
-            </iframe>
-            </div>
-        
+            <Header />
+            <Head>
+                <script src="https://www.amilia.com/scripts/amilia-iframe.js" type="text/javascript"></script>
+            </Head>
 
-        </section>
-        <Footer/>
+
+            <section className="banner section has-text-centered" style={{ height: "80vh" }}>
+
+                <div id="amilia">
+                    <div id="amilia"><iframe id="amilia-iframe" allowtransparency="true" frameborder="0" width="100%" style="width:100%!important; border:none!important; overflow:hidden!important; visibility:hidden;" scrolling="no" horizontalscrolling="no" verticalscrolling="no" data-color-code="#2B91E1" src="https://www.amilia.com/store/en/urbanelementzone/shop/programs"></iframe><script src="https://www.amilia.com/scripts/amilia-iframe.js" type="text/javascript"></script></div>
+                </div>
+
+
+            </section>
+            <Footer />
         </>
-        
+
     )
 }
